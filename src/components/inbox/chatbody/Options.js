a462a21c78ad342805d3eb89f5cbf3345f3f8c76
Mahfuz Alam <hasibul112002@gmail.com>
2023-11-26 17:44:37 +0600

    Add message input and submit functionality to Options component

--- src/components/inbox/chatbody/Options.js
+++ src/components/inbox/chatbody/Options.js
@@ -1,12 +1,49 @@
-export default function Options() {
+import { useEffect, useState } from "react";
+import { useAddConversationMutation } from "../../../features/conversations/conversationsApi";
+import { useSelector } from "react-redux";
+
+export default function Options({info}) {
+
+    const [message, setMessage] = useState("");
+
+    const { user: loggedInUser } = useSelector((state) => state.auth) || {};
+
+    const perticipantEmail = info.receiver.email !== loggedInUser.email ? info.receiver : info.sender;
+
+    const [addConversation, { isSuccess}] =
+    useAddConversationMutation();
+
+    useEffect(() => {
+        if (isSuccess) {
+            setMessage("");
+        }
+    }, [isSuccess]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        addConversation({
+            sender: loggedInUser?.email,
+            data: {
+                participants: `${loggedInUser?.email},${perticipantEmail.email}`,
+                message,
+                timestamp: new Date().getTime(),
+            },
+        });
+
+        console.log(message);
+    };
+
     return (
-        <div className="flex items-center justify-between w-full p-3 border-t border-gray-300">
+        <form onSubmit={handleSubmit} className="flex items-center justify-between w-full p-3 border-t border-gray-300">
             <input
                 type="text"
                 placeholder="Message"
                 className="block w-full py-2 pl-4 mx-3 bg-gray-100 focus:ring focus:ring-violet-500 rounded-full outline-none focus:text-gray-700"
                 name="message"
                 required
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
             />
             <button type="submit">
                 <svg
@@ -18,6 +55,6 @@ export default function Options() {
                     <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z" />
                 </svg>
             </button>
-        </div>
+        </form>
     );
 }
